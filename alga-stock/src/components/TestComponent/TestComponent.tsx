9a0e160f6580c968d630464e39ba756b3100b9b1
Thiago Henrique <thiago.henrique.25@hotmail.com>
2023-10-14 17:54:31 -0400

    entendendo o hook useEffect

--- alga-stock/src/components/TestComponent/TestComponent.tsx
+++ alga-stock/src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './TestComponent.css'
 
 function TestComponent (props: {  name:string }) {
 
     const [age, setAge] = useState(21)
+    
+    useEffect(() => {
+        console.log('Componente was created')
+
+    }, [])
+
+    useEffect(() => {
+     console.log('Age has benn update to: ' + age)
+    }, [age])
     
     return <div className="TesteComponent">
      Olá, { props.name}, {age}
