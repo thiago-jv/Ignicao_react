6917cca0e9d393f7592ad39255af5697837760cc
Thiago Henrique <thiago.henrique.25@hotmail.com>
2023-10-16 19:04:21 -0400

    limpando e add table

--- alga-stock/src/components/App/App.tsx
+++ alga-stock/src/components/App/App.tsx
@@ -1,24 +1,13 @@
 import './App.css';
 import Header from '../Header';
-import Button from '../Shared/Button';
 import Container from '../Container';
-import Input from '../Shared/Input';
 
 function App() {
   return (
     <div className="App">
       <Header title='AlgaStock'/>
-      
       <Container>
-      <ul>
-          {
-            ['Daniel', 'William', 'Thiago', 'Daniel'].map((name, index) => {
-              return <li key={index}>
-                { name }
-              </li>
-            })
-          }
-        </ul>
+      
       </Container>
       
     </div>
