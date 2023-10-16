21d1afbac8c7eeeafaf7f3cfc6d70db8aacc3fc6
Thiago Henrique <thiago.henrique.25@hotmail.com>
2023-10-16 19:10:37 -0400

    estrutura tabela

--- alga-stock/src/components/App/App.tsx
+++ alga-stock/src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from '../Header';
 import Container from '../Container';
+import Table from '../Table';
 
 function App() {
   return (
     <div className="App">
       <Header title='AlgaStock'/>
       <Container>
-      
+       <Table/>
       </Container>
       
     </div>
