8aa3ee835011fcbb3c1446b51eb976d9eddc4239
Thiago Henrique <thiago.henrique.25@hotmail.com>
2023-10-24 20:19:32 -0400

    add formulario

--- alga-stock/src/components/App/App.tsx
+++ alga-stock/src/components/App/App.tsx
@@ -3,6 +3,9 @@ import Header from '../Header';
 import Container from '../Container';
 import Table, { TableHeader } from '../Shared/Table';
 import Products from '../Shared/Table/Table.mockdata';
+import Form from '../Shared/Form';
+import Input from '../Shared/Input';
+import Button from '../Shared/Button';
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -20,6 +23,29 @@ function App() {
           headers={headers}
           data={Products}
         />
+
+<Form title="Product form" onSubmit={console.log}>
+          <Input
+            label="Name"
+            placeholder="E.g.: Cookie"
+          />
+          <Input
+            label="Price"
+            type="number"
+            step="0.01"
+            min="0"
+            placeholder="E.g.: 1.25"
+          />
+          <Input
+            label="Stock"
+            type="number"
+            min="0"
+            placeholder="E.g.: 15"
+          />
+          <Button>
+            Submit
+          </Button>
+        </Form>
       </Container>
     </div>
   );
